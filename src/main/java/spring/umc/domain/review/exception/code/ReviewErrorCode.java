34b49aec1fc35c1cf402fc78ce8d467a1497ba4c
handagyeong <handakyeng@naver.com>
2025-11-12 15:45:11 +0900

    feat: 7주차 과제 수정

--- src/main/java/spring/umc/domain/review/exception/code/ReviewErrorCode.java
+++ src/main/java/spring/umc/domain/review/exception/code/ReviewErrorCode.java
@@ -1,13 +1,12 @@
-package spring.umc.domain.review.exception.code; // <- 리뷰 도메인 패키지에!
-
+package spring.umc.domain.review.exception.code;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-import spring.umc.global.apiPayload.code.BaseErrorCode; // <- ★★★
+import spring.umc.global.apiPayload.code.BaseErrorCode;
 
 @Getter
 @AllArgsConstructor
-public enum ReviewErrorCode implements BaseErrorCode { // <- ★★★ BaseErrorCode 구현!
+public enum ReviewErrorCode implements BaseErrorCode {
 
 
     REVIEW_SEARCH_TYPE_INVALID(HttpStatus.BAD_REQUEST, "REVIEW4001", "유효하지 않은 검색 타입입니다. ('location', 'star', 'both'만 가능)"),
